48e2080bd271ead7ab65aebffd136e4c755da2f6
Белополов Николай <nickforspam@gmail.com>
2014-06-22 00:24:40 +0400

    не законченная функция getID

--- templates/atomic/index.php
+++ templates/atomic/index.php
@@ -906,7 +906,7 @@ Added by bNick at 18.06.2014
 function GetBanner($db, $banner_type){
     $getbanner = array(
         //ToDo: Дефолтные значения должны браться со странницы "баннеры"
-        "url" => "/#",
+        "url" => "/",
         "src" => "banner.jpg"
     );
 
@@ -920,26 +920,26 @@ function GetBanner($db, $banner_type){
     }
     else{
         //Проверяем наличие ссылки на баннер в разделе "страны"
-        $query = "SELECT * FROM d0y13_ray_cantry  WHERE id ='" . $_REQUEST["id"] . "' ";
+        $query = "SELECT * FROM d0y13_ray_cantry  WHERE id ='" . GetID()["countryID"] . "' ";
         $db->setQuery($query);
         $row = $db->loadObject();
 //        if (isset($row->banner1)) {
         if (!empty($row)) {
             switch($banner_type){
                 case 0:
-                    $getbanner["url"] = !empty($row->url1) ? $row->url1 : "/#";
+                    $getbanner["url"] = !empty($row->url1) ? $row->url1 : "/";
                     $getbanner["src"] = !empty($row->banner1) ? $row->banner1 : "banner.jpg";
                     break;
                 case 1:
-                    $getbanner["url"] = !empty($row->url2) ? $row->url2 : "/#";
+                    $getbanner["url"] = !empty($row->url2) ? $row->url2 : "/";
                     $getbanner["src"] = !empty($row->banner2) ? $row->banner2 : "banner.jpg";
                     break;
                 case 2:
-                    $getbanner["url"] = !empty($row->url3) ? $row->url3 : "/#";
+                    $getbanner["url"] = !empty($row->url3) ? $row->url3 : "/";
                     $getbanner["src"] = !empty($row->banner3) ? $row->banner3 : "banner.jpg";
                     break;
                 case 3:
-                    $getbanner["url"] = !empty($row->url4) ? $row->url4 : "/#";
+                    $getbanner["url"] = !empty($row->url4) ? $row->url4 : "/";
                     $getbanner["src"] = !empty($row->banner4) ? $row->banner4 : "banner.jpg";
                     break;
             }
@@ -947,5 +947,47 @@ function GetBanner($db, $banner_type){
         }
     }
     return $getbanner;
+}
+function GetID(){
+    switch ($_REQUEST["view"]){
+        case "cantry":
+            $countryID = $_REQUEST["id"];
+            $regionID = null;
+            break;
+        case "region":
+            $countryID = $_REQUEST["cantry"];
+            $regionID = $_REQUEST["id"];;
+            break;
+    }
+    return array("countryID" => $countryID, "regionID" => $regionID);
+}
+
+function GetRegionBanners($db, $banner_type){
+    //Проверяем наличие ссылки на баннер в разделе "страны"
+    $query = "SELECT * FROM d0y13_ray_cantry  WHERE id ='" . $_REQUEST["id"] . "' ";
+    $db->setQuery($query);
+    $row = $db->loadObject();
+//        if (isset($row->banner1)) {
+    if (!empty($row)) {
+        switch($banner_type){
+            case 0:
+                $getbanner["url"] = !empty($row->url1) ? $row->url1 : "/";
+                $getbanner["src"] = !empty($row->banner1) ? $row->banner1 : "banner.jpg";
+                break;
+            case 1:
+                $getbanner["url"] = !empty($row->url2) ? $row->url2 : "/";
+                $getbanner["src"] = !empty($row->banner2) ? $row->banner2 : "banner.jpg";
+                break;
+            case 2:
+                $getbanner["url"] = !empty($row->url3) ? $row->url3 : "/";
+                $getbanner["src"] = !empty($row->banner3) ? $row->banner3 : "banner.jpg";
+                break;
+            case 3:
+                $getbanner["url"] = !empty($row->url4) ? $row->url4 : "/";
+                $getbanner["src"] = !empty($row->banner4) ? $row->banner4 : "banner.jpg";
+                break;
+        }
+
+    }
 }
 ?>
