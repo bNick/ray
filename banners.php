219ed472b426938779e0674ac5532cbddc02138a
Белополов Николай <nickforspam@gmail.com>
2014-07-04 00:24:59 +0400

    вывод баннера для конечной страницы

--- banners.php
+++ banners.php
@@ -8,9 +8,11 @@
 
 defined('_JEXEC') or die('Restricted access');
 
+define('DEFAULT_URL', $_SERVER["REQUEST_URI"]);
+
 function GetBanner($db, $banner_type){
     $getbanner = array(
-        "url" => "/",
+        "url" => DEFAULT_URL,
         "src" => "banner.jpg"
     );
     $ids = GetID();
@@ -21,18 +23,22 @@ function GetBanner($db, $banner_type){
         $getbanner = CountryBanner($db, $banner_type, $ids, $getbanner);
         $getarticlebanner = ArticleBanner($db, $banner_type, $getbanner);
 
-        $getbanner["url"] = ($getarticlebanner["url"] == "/") ? $getbanner["url"] : $getarticlebanner["url"];
+        $getbanner["url"] = ($getarticlebanner["url"] == DEFAULT_URL) ? $getbanner["url"] : $getarticlebanner["url"];
         $getbanner["src"] = ($getarticlebanner["src"] == "banner.jpg") ? $getbanner["src"] : $getarticlebanner["src"];
-    }elseif (IsRegionPage()){
-        $getregionbanner = RegionBanner($db, $banner_type, $ids, $getbanner);
-        $getbanner = CountryBanner($db, $banner_type, $ids, $getbanner);
+    }else{
+        $getcountrybanner = CountryBanner($db, $banner_type, $ids, $getbanner);
+        $getbanner = RegionBanner($db, $banner_type, $ids, $getbanner);
         $getarticlebanner = ArticleBanner($db, $banner_type, $getbanner);
 
-
-        $getbanner["url"] = ($getarticlebanner["url"] == "/") ? $getbanner["url"] : $getregionbanner["url"];
-        $getbanner["src"] = ($getarticlebanner["src"] == "banner.jpg") ? $getbanner["src"] : $getregionbanner["src"];
-        $getbanner["url"] = ($getarticlebanner["url"] == "/") ? $getbanner["url"] : $getarticlebanner["url"];
+        $getbanner["url"] = ($getarticlebanner["url"] == DEFAULT_URL) ? $getbanner["url"] : $getcountrybanner["url"];
+        $getbanner["src"] = ($getarticlebanner["src"] == "banner.jpg") ? $getbanner["src"] : $getcountrybanner["src"];
+        $getbanner["url"] = ($getarticlebanner["url"] == DEFAULT_URL) ? $getbanner["url"] : $getarticlebanner["url"];
         $getbanner["src"] = ($getarticlebanner["src"] == "banner.jpg") ? $getbanner["src"] : $getarticlebanner["src"];
+
+        $getbanner["url"] = ($getbanner["url"] == DEFAULT_URL) ? $getcountrybanner["url"] : $getbanner["url"];
+        $getbanner["src"] = ($getbanner["src"] == "banner.jpg") ? $getcountrybanner["src"] : $getbanner["src"];
+
+
     }
     return $getbanner;
 }
@@ -46,6 +52,18 @@ function GetID(){
         case "region":
             $countryID = $_REQUEST["cantry"];
             $regionID = $_REQUEST["id"];;
+            break;
+        case "naspunkt":
+            $countryID = $_REQUEST["cantry"];
+            $regionID = $_REQUEST["region"];;
+            break;
+        case "servis":
+            $countryID = $_REQUEST["cantry"];
+            $regionID = $_REQUEST["region"];;
+            break;
+        case "item":
+            $countryID = $_REQUEST["cantry"];
+            $regionID = $_REQUEST["region"];;
             break;
         default:
             $countryID = null;
@@ -72,7 +90,7 @@ function ArticleBanner($db, $banner_type, $getbanner){
     if ($row = $db->loadObject()) {
 //        $getbanner["url"] = $row->url;
 //        $getbanner["src"] = $row->object;
-        $getbanner["url"] = !empty($row->url) ? $row->url : "/";
+        $getbanner["url"] = !empty($row->url) ? $row->url : DEFAULT_URL;
         $getbanner["src"] = !empty($row->object) ? $row->object : "banner.jpg";
     }
     return $getbanner;
@@ -86,19 +104,19 @@ function CountryBanner($db, $banner_type, $ids, $getbanner){
     if (!empty($row)) {
         switch($banner_type){
             case 0:
-                $getbanner["url"] = !empty($row->url1) ? $row->url1 : "/";
+                $getbanner["url"] = !empty($row->url1) ? $row->url1 : DEFAULT_URL;
                 $getbanner["src"] = !empty($row->banner1) ? $row->banner1 : "banner.jpg";
                 break;
             case 1:
-                $getbanner["url"] = !empty($row->url2) ? $row->url2 : "/";
+                $getbanner["url"] = !empty($row->url2) ? $row->url2 : DEFAULT_URL;
                 $getbanner["src"] = !empty($row->banner2) ? $row->banner2 : "banner.jpg";
                 break;
             case 2:
-                $getbanner["url"] = !empty($row->url3) ? $row->url3 : "/";
+                $getbanner["url"] = !empty($row->url3) ? $row->url3 : DEFAULT_URL;
                 $getbanner["src"] = !empty($row->banner3) ? $row->banner3 : "banner.jpg";
                 break;
             case 3:
-                $getbanner["url"] = !empty($row->url4) ? $row->url4 : "/";
+                $getbanner["url"] = !empty($row->url4) ? $row->url4 : DEFAULT_URL;
                 $getbanner["src"] = !empty($row->banner4) ? $row->banner4 : "banner.jpg";
                 break;
         }
@@ -106,29 +124,29 @@ function CountryBanner($db, $banner_type, $ids, $getbanner){
     return $getbanner;
 }
 function RegionBanner($db, $banner_type, $ids, $getbanner){
-    $query = "SELECT * FROM 0y13_ray_region  WHERE id ='" . $ids["countryID"] . "' ";
+    $query = "SELECT * FROM 0y13_ray_region  WHERE cantry ='" . $ids["countryID"] . "' and id='" . $ids["regionID"] . "'";
     $db->setQuery($query);
     $row = $db->loadObject();
 //        if (isset($row->banner1)) {
     if (!empty($row)) {
         switch($banner_type){
             case 0:
-                $getbanner["url"] = !empty($row->url1) ? $row->url1 : "/";
+                $getbanner["url"] = !empty($row->url1) ? $row->url1 : DEFAULT_URL;
                 $getbanner["src"] = !empty($row->banner1) ? $row->banner1 : "banner.jpg";
                 break;
             case 1:
-                $getbanner["url"] = !empty($row->url2) ? $row->url2 : "/";
+                $getbanner["url"] = !empty($row->url2) ? $row->url2 : DEFAULT_URL;
                 $getbanner["src"] = !empty($row->banner2) ? $row->banner2 : "banner.jpg";
                 break;
             case 2:
-                $getbanner["url"] = !empty($row->url3) ? $row->url3 : "/";
+                $getbanner["url"] = !empty($row->url3) ? $row->url3 : DEFAULT_URL;
                 $getbanner["src"] = !empty($row->banner3) ? $row->banner3 : "banner.jpg";
                 break;
             case 3:
-                $getbanner["url"] = !empty($row->url4) ? $row->url4 : "/";
+                $getbanner["url"] = !empty($row->url4) ? $row->url4 : DEFAULT_URL;
                 $getbanner["src"] = !empty($row->banner4) ? $row->banner4 : "banner.jpg";
                 break;
         }
-
     }
+    return $getbanner;
 }
